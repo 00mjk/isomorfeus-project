c135bf7d02532a08ea1e39456cd0c1abac4e464f
Jan Biedermann <jan@kursator.de>
2019-08-18 02:23:05 +0200

    Move Anonymous and its Policy to i-policy.

--- lib/anonymous.rb
+++
@@ -1,6 +0,0 @@
-class Anonymous < BasicObject
-  # policy methods get added by Isomorfeus::Transport::AnonymousPolicy
-  def id
-    'anonymous'
-  end
-end
--- lib/isomorfeus/transport/anonymous_policy.rb
+++
@@ -1,8 +0,0 @@
-module Isomorfeus
-  module Transport
-    class AnonymousPolicy < LucidPolicy::Base
-      policy_for Anonymous
-      deny all
-    end
-  end
-end
