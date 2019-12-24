820edc5460666b9c50dee168b30fe0d391b19463
Jan Biedermann <jan@kursator.de>
2019-12-24 20:46:38 +0100

    Fix finders collection access

--- ruby/isomorfeus-data/lib/isomorfeus_data/lucid_data/graph/finders.rb
+++ ruby/isomorfeus-data/lib/isomorfeus_data/lucid_data/graph/finders.rb
@@ -2,7 +2,7 @@ module LucidData
   module Graph
     module Finders
       def find_node(attribute_hash = nil, &block)
-        @_node_collections.each_value do |collection|
+        node_collections.each_value do |collection|
           node = collection.find(attribute_hash, &block)
           return node if node
         end
@@ -13,7 +13,7 @@ module LucidData
 
       def find_nodes(attribute_hash = nil, &block)
         found_nodes = []
-        @_node_collections.each_value do |collection|
+        node_collections.each_value do |collection|
           nodes = collection.find_all(attribute_hash, &block)
           found_nodes.push(*nodes)
         end
@@ -24,7 +24,7 @@ module LucidData
       alias find_vertices find_nodes
 
       def find_node_by_key(node_key)
-        @_node_collections.each_value do |collection|
+        node_collections.each_value do |collection|
           node = collection.find_by_key(node_key)
           return node if node
         end
@@ -34,7 +34,7 @@ module LucidData
       alias find_vertex_by_key find_node_by_key
 
       def find_node_by_sid(node)
-        @_node_collections.each_value do |collection|
+        node_collections.each_value do |collection|
           node = collection.find_by_sid(node)
           return node if node
         end
@@ -44,7 +44,7 @@ module LucidData
       alias find_vertex_by_sid find_node_by_sid
 
       def find_edge(attribute_hash = nil, &block)
-        @_edge_collections.each_value do |collection|
+        edge_collections.each_value do |collection|
           edge = collection.find(attribute_hash, &block)
           return edge if edge
         end
@@ -54,7 +54,7 @@ module LucidData
 
       def find_edges(attribute_hash = nil, &block)
         found_edges = []
-        @_edge_collections.each_value do |collection|
+        edge_collections.each_value do |collection|
           edges = collection.find_all(attribute_hash, &block)
           found_edges.push(*edges)
         end
@@ -63,7 +63,7 @@ module LucidData
       alias find_links find_edges
 
       def find_edge_by_key(edge_key)
-        @_edge_collections.each_value do |collection|
+        edge_collections.each_value do |collection|
           edge = collection.find_by_key(edge_key)
           return edge if edge
         end
@@ -72,7 +72,7 @@ module LucidData
       alias find_link_by_key find_edge_by_key
 
       def find_edge_by_sid(edge)
-        @_edge_collections.each_value do |collection|
+        edge_collections.each_value do |collection|
           edge = collection.find_by_sid(edge)
           return edge if edge
         end
@@ -81,7 +81,7 @@ module LucidData
       alias find_link_by_key find_edge_by_key
 
       def find_edge_by_from(node)
-        @_edge_collections.each_value do |collection|
+        edge_collections.each_value do |collection|
           edge = collection.find_by_from(node)
           return edge if edge
         end
@@ -90,7 +90,7 @@ module LucidData
       alias find_link_by_from find_edge_by_from
 
       def find_edge_by_to(node)
-        @_edge_collections.each_value do |collection|
+        edge_collections.each_value do |collection|
           edge = collection.find_by_to(node)
           return edge if edge
         end
@@ -99,7 +99,7 @@ module LucidData
       alias find_link_by_to find_edge_by_to
 
       def find_edge_by_target(node)
-        @_edge_collections.each_value do |collection|
+        edge_collections.each_value do |collection|
           edge = collection.find_by_target(node)
           return edge if edge
         end
@@ -109,7 +109,7 @@ module LucidData
 
       def find_edges_by_from(node)
         found_edges = []
-        @_edge_collections.each_value do |collection|
+        edge_collections.each_value do |collection|
           coll_edges = collection.find_all_by_from(node)
           found_edges.push(*coll_edges)
         end
@@ -119,7 +119,7 @@ module LucidData
 
       def find_edges_by_to(node)
         found_edges = []
-        @_edge_collections.each_value do |collection|
+        edge_collections.each_value do |collection|
           coll_edges = collection.find_all_by_to(node)
           found_edges.push(*coll_edges)
         end
@@ -129,7 +129,7 @@ module LucidData
 
       def find_edges_by_target(node)
         found_edges = []
-        @_edge_collections.each_value do |collection|
+        edge_collections.each_value do |collection|
           coll_edges = collection.find_all_by_target(node)
           found_edges.push(*coll_edges)
         end
