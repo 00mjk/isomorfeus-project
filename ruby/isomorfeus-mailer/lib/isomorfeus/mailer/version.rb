88c5681e93d0754dd5ecf5b1edec90c89ef54f0b
Jan Biedermann <jan@kursator.de>
2020-02-10 17:30:03 +0100

    Version 1.0.0.zeta15

--- ruby/isomorfeus-mailer/lib/isomorfeus/mailer/version.rb
+++ ruby/isomorfeus-mailer/lib/isomorfeus/mailer/version.rb
@@ -1,5 +1,5 @@
 module Isomorfeus
   module Mailer
-    VERSION = '1.0.0.zeta14'
+    VERSION = '1.0.0.zeta15'
   end
 end
--- ruby/isomorfeus/lib/isomorfeus/version.rb
+++ ruby/isomorfeus/lib/isomorfeus/version.rb
@@ -1,3 +1,3 @@
 module Isomorfeus
-  VERSION = '1.0.0.zeta14'
+  VERSION = '1.0.0.zeta15'
 end
