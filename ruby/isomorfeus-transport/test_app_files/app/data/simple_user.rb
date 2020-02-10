9d8f2e45fba927540d0f9affddddde2403429f3b
Jan Biedermann <jan@kursator.de>
2020-02-10 16:29:36 +0100

    Guard RUBY_ENGINE in example.

--- ruby/isomorfeus-transport/test_app_files/app/data/simple_user.rb
+++ ruby/isomorfeus-transport/test_app_files/app/data/simple_user.rb
@@ -2,8 +2,10 @@ class SimpleUser < LucidData::Document::Base
   include LucidAuthentication::Mixin
 
   execute_login do |user:, pass:|
-    if user == 'joe_simple' && pass == 'my_pass'
-      SimpleUser.new(key: '123')
+    if RUBY_ENGINE != 'opal'
+      if user == 'joe_simple' && pass == 'my_pass'
+        SimpleUser.new(key: '123')
+      end
     end
   end
 end
