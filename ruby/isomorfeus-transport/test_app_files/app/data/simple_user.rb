b89cb3828b63eabe55210e9c24fcf09c542f4688
Jan Biedermann <jan@kursator.de>
2020-02-10 15:56:00 +0100

    Use execute_login

--- ruby/isomorfeus-transport/test_app_files/app/data/simple_user.rb
+++ ruby/isomorfeus-transport/test_app_files/app/data/simple_user.rb
@@ -1,7 +1,7 @@
 class SimpleUser < LucidData::Document::Base
   include LucidAuthentication::Mixin
 
-  authentication do |user:, pass:|
+  execute_login do |user:, pass:|
     if user == 'joe_simple' && pass == 'my_pass'
       SimpleUser.new(key: '123')
     end
