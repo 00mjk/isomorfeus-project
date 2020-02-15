7a6268156d01fb3487e543b0a73b0adec4961c36
Jan Biedermann <jan@kursator.de>
2020-02-15 04:46:55 +0100

    Dont pass props as always nil.

--- ruby/isomorfeus-policy/lib/isomorfeus_policy/lucid_authorization/mixin.rb
+++ ruby/isomorfeus-policy/lib/isomorfeus_policy/lucid_authorization/mixin.rb
@@ -9,7 +9,7 @@ module LucidAuthorization
         policy_class = nil
       end
       return false unless policy_class
-      policy_class.new(self).authorized?(target_class, target_method, props = nil)
+      policy_class.new(self).authorized?(target_class, target_method, props)
     end
 
     def authorized!(target_class, target_method = nil, props = nil)
@@ -17,7 +17,7 @@ module LucidAuthorization
       class_name = class_name.split('>::').last if class_name.start_with?('#<')
       policy_class = Isomorfeus.cached_policy_class("#{class_name}Policy")
       Isomorfeus.raise_error(error_class: LucidPolicy::Exception, message: "#{self}: policy class #{class_name}Policy not found!") unless policy_class
-      policy_class.new(self).authorized!(target_class, target_method, props = nil)
+      policy_class.new(self).authorized!(target_class, target_method, props)
     end
   end
 end
