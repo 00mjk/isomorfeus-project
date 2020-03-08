a0979bc180e2fffacccd64555ce6011637ca2ba2
Jan Biedermann <jan@kursator.de>
2020-03-08 15:16:11 +0100

    Update test_apps.

---
+++ ruby/isomorfeus-transport/test_app_files/app/server/test_app_roda_app.rb
@@ -0,0 +1,62 @@
+STDERR.puts "config", File.expand_path(File.join(__dir__, '..', '..', 'config'))
+Isomorfeus.load_configuration(File.expand_path(File.join(__dir__, '..', '..', 'config')))
+
+class TestAppRodaApp < Roda
+  extend Isomorfeus::Transport::Middlewares
+  include OpalWebpackLoader::ViewHelper
+  include Isomorfeus::ReactViewHelper
+
+  use_isomorfeus_middlewares
+  plugin :public, root: 'public'
+  plugin :render, views: File.join(__dir__, '..', 'layouts'), cache: Isomorfeus.production?
+
+  def locale
+    env.http_accept_language.preferred_language_from(Isomorfeus.available_locales) ||
+      env.http_accept_language.compatible_language_from(Isomorfeus.available_locales) ||
+      Isomorfeus.locale
+  end
+
+  def page_content(env, location)
+    mount_component('TestAppApp',{ location_host: env['HTTP_HOST'], location: location, locale: locale }, 'web_ssr.js')
+  end
+
+  route do |r|
+    r.root do
+      content = page_content(env, '/')
+      response.status = ssr_response_status
+      render('web', locals: { content: content, title: 'TestAppApp', script_tag: owl_script_tag('web.js') })
+    end
+
+    r.public
+
+    r.get 'favicon.ico' do
+      r.public
+    end
+
+    r.get 'robots.txt' do
+      r.public
+    end
+
+    unless Isomorfeus.production?
+      r.on 'mail_preview', String do |component_name|
+        component_class = component_name.camelize
+        props = { location_host: env['HTTP_HOST'], location: '/mail_preview', locale: locale }.merge(r.params)
+        content = mount_component(component_class, props, 'mail_components.js')
+        response.status = ssr_response_status
+        render('web', locals: { content: content, component_class: component_class })
+      end
+    end
+
+    r.get 'ssr' do
+      content = page_content(env, env['PATH_INFO'])
+      response.status = ssr_response_status
+      render('ssr', locals: { content: content, title: 'TestAppApp' })
+    end
+
+    r.get do
+      content = page_content(env, env['PATH_INFO'])
+      response.status = ssr_response_status
+      render('web', locals: { content: content, title: 'TestAppApp', script_tag: owl_script_tag('web.js') })
+    end
+  end
+end
