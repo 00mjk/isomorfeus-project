ff77b79e17e9068896da050dcae427862c855061
Jan Biedermann <jan@kursator.de>
2020-03-08 14:55:20 +0100

    Update data test_app .

--- ruby/isomorfeus-data/test_app_files/app/server/test_app_roda_app.rb
+++ ruby/isomorfeus-data/test_app_files/app/server/test_app_roda_app.rb
@@ -1,4 +1,5 @@
-Isomorfeus.load_configuration
+STDERR.puts "config", File.expand_path(File.join(__dir__, '..', '..', 'config'))
+Isomorfeus.load_configuration(File.expand_path(File.join(__dir__, '..', '..', 'config')))
 
 class TestAppRodaApp < Roda
   extend Isomorfeus::Transport::Middlewares
@@ -7,27 +8,23 @@ class TestAppRodaApp < Roda
 
   use_isomorfeus_middlewares
   plugin :public, root: 'public'
+  plugin :render, views: File.join(__dir__, '..', 'layouts'), cache: Isomorfeus.production?
+
+  def locale
+    env.http_accept_language.preferred_language_from(Isomorfeus.available_locales) ||
+      env.http_accept_language.compatible_language_from(Isomorfeus.available_locales) ||
+      Isomorfeus.locale
+  end
 
   def page_content(env, location)
-    locale = env.http_accept_language.preferred_language_from(Isomorfeus.available_locales)
-    locale = env.http_accept_language.compatible_language_from(Isomorfeus.available_locales) unless locale
-    locale = Isomorfeus.locale unless locale
-    <<~HTML
-      <html>
-        <head>
-          <title>Welcome to TestAppApp</title>
-          #{owl_script_tag 'application.js'}
-        </head>
-        <body>
-          #{mount_component('TestAppApp', location_host: env['HTTP_HOST'], location: location, locale: locale)}
-        </body>
-      </html>
-    HTML
+    mount_component('TestAppApp',{ location_host: env['HTTP_HOST'], location: location, locale: locale }, 'web_ssr.js')
   end
 
   route do |r|
     r.root do
-      page_content(env, '/')
+      content = page_content(env, '/')
+      response.status = ssr_response_status
+      render('web', locals: { content: content, title: 'TestAppApp', script_tag: owl_script_tag('web.js') })
     end
 
     r.public
@@ -36,27 +33,30 @@ class TestAppRodaApp < Roda
       r.public
     end
 
+    r.get 'robots.txt' do
+      r.public
+    end
+
+    unless Isomorfeus.production?
+      r.on 'mail_preview', String do |component_name|
+        component_class = component_name.camelize
+        props = { location_host: env['HTTP_HOST'], location: '/mail_preview', locale: locale }.merge(r.params)
+        content = mount_component(component_class, props, 'mail_components.js')
+        response.status = ssr_response_status
+        render('web', locals: { content: content, component_class: component_class })
+      end
+    end
+
     r.get 'ssr' do
-      locale = env.http_accept_language.preferred_language_from(Isomorfeus.available_locales)
-      locale = env.http_accept_language.compatible_language_from(Isomorfeus.available_locales) unless locale
-      locale = Isomorfeus.locale unless locale
-      <<~HTML
-        <html>
-          <head>
-            <title>Welcome to TestAppApp</title>
-          </head>
-          <body>
-            #{mount_component('TestAppApp', location_host: env['HTTP_HOST'], location: env['PATH_INFO'], locale: locale)}
-            <div id="test_anchor"></div>
-          </body>
-        </html>
-      HTML
+      content = page_content(env, env['PATH_INFO'])
+      response.status = ssr_response_status
+      render('ssr', locals: { content: content, title: 'TestAppApp' })
     end
 
     r.get do
       content = page_content(env, env['PATH_INFO'])
       response.status = ssr_response_status
-      content
+      render('web', locals: { content: content, title: 'TestAppApp', script_tag: owl_script_tag('web.js') })
     end
   end
 end
