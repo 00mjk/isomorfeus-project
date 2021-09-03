76a48c04b5839b18a224cdc3e8ac19a2be4d93b7
Jan Biedermann <jan@kursator.de>
2021-09-04 00:04:52 +0200

    adjust targets and remove yarn

--- isomorfeus/lib/isomorfeus/cli.rb
+++ isomorfeus/lib/isomorfeus/cli.rb
@@ -8,15 +8,14 @@ module Isomorfeus
 Select targets to install.
 Available targets:
   web: Create web applications
-  native: Create applications with React Native (Requires #{'Isomorfeus Professional and the' unless Isomorfeus::Installer.is_professional} web target)'
-  all: web + native
+  all: web
 Example:
   -t all          - Setup project for all targets
   -t "web native" - Setup project for all targets
   -t web          - Setup project for web applications only
 DESC
     option :yarn_and_bundle, default: 'yes', required: false, type: :string, aliases: '-y',
-           desc: "Execute yarn install and bundle install: -y or -y yes, do not execute: -y no"
+           desc: "Execute bundle install: -y or -y yes, do not execute: -y no"
     def new(project_name)
       Isomorfeus::Installer.set_project_names(project_name)
       Isomorfeus::Installer.options = options
@@ -40,25 +39,25 @@ DESC
       Dir.chdir('..')
     end
 
-    desc "add_target target", "Add a target to a existing Isomorfeus Project."
-    long_desc <<~DESC
-Add a target to a existing Isomorfeus Project.
-\x5
-\x5Available targets:
-\x5  native: Create applications with React Native (Requires #{'Isomorfeus Professional and the' unless Isomorfeus::Installer.is_professional} web target)'
-\x5
-\x5  Example:
-\x5  isomorfeus add_target native - Add mobile as target to a existing project
-DESC
-    option :yarn_and_bundle, default: 'yes', required: false, type: :string, aliases: '-y',
-           desc: "Execute yarn install and bundle install: -y or -y yes, do not execute: -y no"
-    def add(project_name)
-      Isomorfeus::Installer.set_project_names(project_name)
-      Isomorfeus::Installer.options = options
-      Isomorfeus::Installer::InstallTargets.execute
-      Isomorfeus::Installer::Gemfile.execute
-      Isomorfeus::Installer::YarnAndBundle.execute
-    end
+#     desc "add_target target", "Add a target to a existing Isomorfeus Project."
+#     long_desc <<~DESC
+# Add a target to a existing Isomorfeus Project.
+# \x5
+# \x5Available targets:
+# \x5  native: Create applications with React Native (Requires #{'Isomorfeus Professional and the' unless Isomorfeus::Installer.is_professional} web target)'
+# \x5
+# \x5  Example:
+# \x5  isomorfeus add_target native - Add mobile as target to a existing project
+# DESC
+#     option :yarn_and_bundle, default: 'yes', required: false, type: :string, aliases: '-y',
+#            desc: "Execute bundle install: -y or -y yes, do not execute: -y no"
+#     def add(project_name)
+#       Isomorfeus::Installer.set_project_names(project_name)
+#       Isomorfeus::Installer.options = options
+#       Isomorfeus::Installer::InstallTargets.execute
+#       Isomorfeus::Installer::Gemfile.execute
+#       Isomorfeus::Installer::YarnAndBundle.execute
+#     end
 
     desc "console", "Open console for current project."
     def console
@@ -67,7 +66,7 @@ DESC
 
     desc "upgrade", "Install updated config and provide hints for upgrading."
     option :yarn_and_bundle, default: 'yes', required: false, type: :string, aliases: '-y',
-           desc: "Execute yarn install and bundle install: -y or -y yes, do not execute: -y no"
+           desc: "Execute bundle install: -y or -y yes, do not execute: -y no"
     def upgrade
       Isomorfeus::Installer::Upgrade.execute
       Isomorfeus::Installer::YarnAndBundle.execute
@@ -76,7 +75,7 @@ DESC
     desc "versions", "Show versions of important Isomorfeus gems"
     def versions
       output = ''
-      %w[arango-driver arango-driver-professional opal-webpack-loader isomorfeus-redux isomorfeus-react isomorfeus-data isomorfeus-i18n isomorfeus-mailer isomorfeus-operation
+      %w[isomorfeus-asset-manager isomorfeus-redux isomorfeus-preact isomorfeus-data isomorfeus-i18n isomorfeus-mailer isomorfeus-operation
          isomorfeus-policy isomorfeus-transport isomorfeus isomorfeus-professional].each do |gem|
         o = `bundle info #{gem} 2>&1`
         o.each_line do |line|
@@ -96,7 +95,7 @@ DESC
     option :rack_server, default: 'iodine', aliases: '-r',
            desc: "Select rack server, one of: #{Isomorfeus::Installer.sorted_rack_servers.join(', ')}. (optional, default: iodine)"
     option :yarn_and_bundle, default: 'yes', required: false, type: :string, aliases: '-y',
-           desc: "Execute yarn install and bundle install: -y or -y yes, do not execute: -y no"
+           desc: "Execute bundle install: -y or -y yes, do not execute: -y no"
     def test_app
       Isomorfeus::Installer.set_project_names('test_app')
       Isomorfeus::Installer.options = options
