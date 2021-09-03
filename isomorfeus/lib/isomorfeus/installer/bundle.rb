3b71df792235b60a9af69edb9e5c6a10b833ad8c
Jan Biedermann <jan@kursator.de>
2021-09-04 01:07:53 +0200

    simplify and fix end

--- isomorfeus/lib/isomorfeus/installer/bundle.rb
+++ isomorfeus/lib/isomorfeus/installer/bundle.rb
@@ -1,14 +1,11 @@
 module Isomorfeus
   module Installer
     class Bundle
-      class << self
-        def execute
-            puts 'Executing bundle install:'
-            bundle_command = Gem.bin_path("bundler", "bundle")
-            Bundler.with_original_env do
-              system("#{Gem.ruby} #{bundle_command} install")
-            end
-          end
+      def self.execute
+        puts 'Executing bundle install:'
+        bundle_command = Gem.bin_path("bundler", "bundle")
+        Bundler.with_original_env do
+          system("#{Gem.ruby} #{bundle_command} install")
         end
       end
     end
