dcbc737612210dbbde5b51d3580e091ae93799df
Jan Biedermann <jan@kursator.de>
2021-09-07 00:01:05 +0200

    fix isomorfeus_version missing

--- isomorfeus/lib/isomorfeus/installer/gemfile.rb
+++ isomorfeus/lib/isomorfeus/installer/gemfile.rb
@@ -19,13 +19,12 @@ module Isomorfeus
           Isomorfeus::Installer.rack_servers[Isomorfeus::Installer.options[:rack_server]]&.fetch(:gems)&.each do |gem|
             rack_server_gems << generate_gem_line(gem)
           end
-          data_hash = { rack_server_gems: rack_server_gems.chop }
+          data_hash = { rack_server_gems: rack_server_gems.chop, isomorfeus_version: "'~> #{Isomorfeus::VERSION}'" }
+
           if Isomorfeus::Installer.source_dir
             %i[isomorfeus isomorfeus_data isomorfeus_i18n isomorfeus_mailer isomorfeus_operation isomorfeus_policy isomorfeus_transport].each do |i_module|
               data_hash[i_module] = i_module == Isomorfeus::Installer.isomorfeus_module ? "path: '..'" : "path: '../../#{i_module.to_s.tr('_', '-')}'"
             end
-          else
-            data_hash[:isomorfeus_version] = "'~> #{Isomorfeus::VERSION}'"
           end
 
           create_file_from_template(Isomorfeus::Installer.templates_path, 'Gemfile.erb', 'Gemfile', data_hash)
