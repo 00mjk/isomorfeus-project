8d61df950ccbd9d22227ebbd9b27bc39b6ba6742
Jan Biedermann <jan@kursator.de>
2021-10-31 16:37:05 +0100

    adapt imports to isomorfeus-preact

--- isomorfeus-mailer/lib/isomorfeus/mailer/imports.rb
+++ isomorfeus-mailer/lib/isomorfeus/mailer/imports.rb
@@ -6,14 +6,6 @@ module Isomorfeus
         Isomorfeus.assets['mail.js'].add_js_import('preact', 'Preact', '*')
         Isomorfeus.assets['mail.js'].add_js_import('preact/hooks', 'PreactHooks', '*')
         Isomorfeus.assets['mail.js'].add_js_import('wouter-preact', nil, ['Router', 'Link', 'Redirect', 'Route', 'Switch'])
-
-        Isomorfeus.assets['mail.js'].add_js_import('nano-css', 'NanoCSS', '*')
-        %w[rule sheet nesting hydrate unitless global keyframes].each do |addon|
-          Isomorfeus.assets['mail.js'].add_js_import("nano-css/addon/#{addon}", 'NanoCSSAddons', 'addon', addon)
-        end
-        Isomorfeus.assets['mail.js'].add_js_import("nano-css/addon/animate/fadeIn", 'NanoCSSAddons', 'addon', 'fadeIn')
-        Isomorfeus.assets['mail.js'].add_js_import("nano-css/addon/animate/fadeOut", 'NanoCSSAddons', 'addon', 'fadeOut')
-
         Isomorfeus.assets['mail.js'].add_js_import('preact-render-to-string', 'Preact', ['render'], nil, 'renderToString')
         Isomorfeus.assets['mail.js'].add_js_import('wouter-preact/static-location', 'staticLocationHook')
         Isomorfeus.assets['mail.js'].add_js_import('ws', 'WebSocket')
