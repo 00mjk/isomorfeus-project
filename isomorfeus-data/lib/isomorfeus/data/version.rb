0504356d738fbdd7fec2abcf990e56eb82dfbd1d
Jan Biedermann <jan@kursator.de>
2021-11-11 01:06:51 +0100

    2.0.0.rc2

--- isomorfeus-data/lib/isomorfeus/data/version.rb
+++ isomorfeus-data/lib/isomorfeus/data/version.rb
@@ -1,5 +1,5 @@
 module Isomorfeus
   module Data
-    VERSION = '2.0.0.rc1'
+    VERSION = '2.0.0.rc2'
   end
 end
--- isomorfeus-mailer/lib/isomorfeus/mailer/version.rb
+++ isomorfeus-mailer/lib/isomorfeus/mailer/version.rb
@@ -1,5 +1,5 @@
 module Isomorfeus
   module Mailer
-    VERSION = '2.0.0.rc1'
+    VERSION = '2.0.0.rc2'
   end
 end
