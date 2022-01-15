e101b89ac652b03abf0807507878998c216d778e
Jan Biedermann <jan@kursator.de>
2022-01-15 15:05:15 +0100

    2.0.0

--- isomorfeus-mailer/lib/isomorfeus/mailer/version.rb
+++ isomorfeus-mailer/lib/isomorfeus/mailer/version.rb
@@ -1,5 +1,5 @@
 module Isomorfeus
   module Mailer
-    VERSION = '2.0.0.rc10'
+    VERSION = '2.0.0'
   end
 end
--- isomorfeus/lib/isomorfeus/version.rb
+++ isomorfeus/lib/isomorfeus/version.rb
@@ -1,3 +1,3 @@
 module Isomorfeus
-  VERSION = '2.0.0.rc10'
+  VERSION = '2.0.0'
 end
