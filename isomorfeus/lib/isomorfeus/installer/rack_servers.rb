815d90b2f81f4b77207d5ba9b1f5598df84fe80b
Jan Biedermann <jan@kursator.de>
2022-01-20 17:14:32 +0100

    update and remove unused

--- isomorfeus/lib/isomorfeus/installer/rack_servers.rb
+++ isomorfeus/lib/isomorfeus/installer/rack_servers.rb
@@ -1,5 +1,4 @@
 Isomorfeus::Installer.add_rack_server('isomorfeus-iodine', {
-  gems: [ { name: 'isomorfeus-iodine', version: "~> 0.7.45", require: true } ],
-  start_command: 'bundle exec iodine',
+  gems: [ { name: 'isomorfeus-iodine', version: "~> 0.7.46", require: true } ],
   config_template: 'iodine.rb.erb'
 })
