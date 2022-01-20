e7bfeea48853f8503e0d3727369e71b180038088
Jan Biedermann <jan@kursator.de>
2022-01-20 17:14:51 +0100

    space

--- isomorfeus/lib/isomorfeus/installer/rack_servers.rb
+++ isomorfeus/lib/isomorfeus/installer/rack_servers.rb
@@ -1,4 +1,4 @@
 Isomorfeus::Installer.add_rack_server('isomorfeus-iodine', {
-  gems: [ { name: 'isomorfeus-iodine', version: "~> 0.7.46", require: true } ],
+  gems: [{ name: 'isomorfeus-iodine', version: "~> 0.7.46", require: true }],
   config_template: 'iodine.rb.erb'
 })
