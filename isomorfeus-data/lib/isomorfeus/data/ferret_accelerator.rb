3cf0a80ed9b5f64663a03d8dc6b372521635445a
Jan Biedermann <jan@kursator.de>
2022-01-24 12:35:00 +0100

    fix ferret accellerator index initialization and id/key mixup

--- isomorfeus-data/lib/isomorfeus/data/ferret_accelerator.rb
+++ isomorfeus-data/lib/isomorfeus/data/ferret_accelerator.rb
@@ -34,17 +34,23 @@ module Isomorfeus
         @index.add_document(document)
       end
 
-      def destroy_doc(id)
-        @index.delete(id)
+      def destroy_doc(key)
+        id = get_doc_id(key)
+        @index.delete(id) if id
         true
       end
 
-      def load_doc(id)
-        @index.doc(id)&.load
+      def load_doc(key)
+        id = get_doc_id(key)
+        @index.doc(id)&.load&.to_h if id
       end
 
-      def save_doc(id, document)
-        @index.update(id, document)
+      def save_doc(key, document)
+        id = get_doc_id(key)
+        if id
+          @index.update(id, document)
+          true
+        end
       end
 
       def search_each(query, options, &block)
@@ -53,13 +59,25 @@ module Isomorfeus
 
       private
 
+      def get_doc_id(key)
+        # special characters must be escaped, characters taken from the ferret query parser documentation
+        escaped_key = key.gsub(/([\\\&\:\(\)\[\]\{\}\!\"\~\^\|\<\>\=\*\?\+\-\s])/, '\\\\\1')
+        top_docs = @index.search("key:\"#{escaped_key}\"", limit: 1)
+        id = top_docs.hits[0].doc if top_docs.total_hits == 1
+      end
+
       def index_path
         File.expand_path(File.join(Isomorfeus.data_documents_path, @doc_class_name_u))
       end
 
       def open_index
         FileUtils.mkdir_p(Isomorfeus.data_documents_path) unless Dir.exist?(Isomorfeus.data_documents_path)
-        @index = Isomorfeus::Ferret::Index::Index.new(path: index_path, id_field: :key)
+        field_infos = Isomorfeus::Ferret::Index::FieldInfos.new(store: :yes, index: :yes, term_vector: :with_positions_offsets)
+        @index = Isomorfeus::Ferret::Index::Index.new(path: index_path, key: :key, auto_flush: true, lock_retry_time: 5)
+        @index.field_infos.add_field(:key, store: :yes, index: :yes, term_vector: :no) unless @index.field_infos[:key]
+        @doc_class.field_options.each do |field, options|
+          @index.field_infos.add_field(field, options) unless @index.field_infos[field]
+        end
       end
     end
   end
