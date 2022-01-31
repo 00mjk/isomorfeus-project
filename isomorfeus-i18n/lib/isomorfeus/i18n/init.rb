ea9f0e1996d3265896b9d700c325b597ddfab4ed
Jan Biedermann <jan@kursator.de>
2022-01-31 04:56:08 +0100

    improve i18n init accuracy

--- isomorfeus-i18n/lib/isomorfeus/i18n/init.rb
+++ isomorfeus-i18n/lib/isomorfeus/i18n/init.rb
@@ -1,45 +1,48 @@
 module Isomorfeus
   module I18n
     class Init
-      if RUBY_ENGINE == 'opal'
-        def self.init
-          return if @initializing || initialized?
-          @initializing = true
-          if Isomorfeus.on_browser?
-            root_element = `document.querySelector('div[data-iso-root]')`
-            Isomorfeus.negotiated_locale = root_element.JS.getAttribute('data-iso-nloc') if root_element
-            init_from_server
-          else
-            init_from_server
-          end
-        end
+      class << self
+        if RUBY_ENGINE == 'opal'
+          attr_accessor :init_promise
 
-        def self.init_from_server
-          Isomorfeus::Transport.promise_send_path('Isomorfeus::I18n::Handler::LocaleHandler', :init, Isomorfeus.negotiated_locale).then do |agent|
-            if agent.processed
-              agent.result
-            else
-              agent.processed = true
-              if agent.response.key?(:error)
-                Isomorfeus.raise_error(message: agent.response[:error])
+          def init
+            return if @initializing || initialized?
+            @initializing = true
+            @initialized = false
+            if Isomorfeus.on_browser?
+              root_element = `document.querySelector('div[data-iso-root]')`
+              Isomorfeus.negotiated_locale = root_element.JS.getAttribute('data-iso-nloc') if root_element
+            end
+            self.init_promise = init_from_server
+          end
+
+          def init_from_server
+            Isomorfeus::Transport.promise_send_path('Isomorfeus::I18n::Handler::LocaleHandler', :init, Isomorfeus.negotiated_locale).then do |agent|
+              if agent.processed
+                agent.result
+              else
+                agent.processed = true
+                if agent.response.key?(:error)
+                  Isomorfeus.raise_error(message: agent.response[:error])
+                end
+                @initializing = false
+                Isomorfeus.store.dispatch(type: 'I18N_LOAD', data: agent.response[:data])
+                @initialized = true
               end
-              @initializing = false
-              Isomorfeus.store.dispatch(type: 'I18N_LOAD', data: agent.response[:data])
             end
           end
-        end
 
-        def self.initialized?
-          result = Redux.fetch_by_path(:i18n_state, :available_locales)
-          result ? true : false
-        end
-      else
-        def self.init
-          FastGettext.add_text_domain(Isomorfeus.i18n_domain, path: Isomorfeus.locale_path, type: Isomorfeus.i18n_type)
-          FastGettext.available_locales = Isomorfeus.available_locales
-          FastGettext.text_domain = Isomorfeus.i18n_domain
-          FastGettext.locale = Isomorfeus.locale
-          Thread.current[:isomorfeus_i18n_initialized] = true
+          def initialized?
+            @initialized
+          end
+        else
+          def init
+            FastGettext.add_text_domain(Isomorfeus.i18n_domain, path: Isomorfeus.locale_path, type: Isomorfeus.i18n_type)
+            FastGettext.available_locales = Isomorfeus.available_locales
+            FastGettext.text_domain = Isomorfeus.i18n_domain
+            FastGettext.locale = Isomorfeus.locale
+            Thread.current[:isomorfeus_i18n_initialized] = true
+          end
         end
       end
     end
