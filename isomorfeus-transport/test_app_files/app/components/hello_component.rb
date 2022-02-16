96af50dd5bf458b49fb0362d071d4a99a87149e4
Jan Biedermann <jan@kursator.de>
2022-02-16 02:51:36 +0100

    instance store is gone

--- isomorfeus-transport/test_app_files/app/components/hello_component.rb
+++ isomorfeus-transport/test_app_files/app/components/hello_component.rb
@@ -2,7 +2,7 @@ class HelloComponent < LucidComponent::Base
   render do
     DIV 'Rendered!'
     DIV "#{class_store.a_value}"
-    DIV "#{store.a_value}"
+    DIV "#{state.a_value}"
     DIV "#{app_store.a_value}"
     NavigationLinks()
   end
