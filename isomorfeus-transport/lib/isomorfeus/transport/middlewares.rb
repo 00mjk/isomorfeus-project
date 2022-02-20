af74521723a47f339b8ec6c7e3d8a791f4d0fff4
Jan Biedermann <jan@kursator.de>
2022-02-20 06:00:56 +0100

    print middleware info only in production

--- isomorfeus-transport/lib/isomorfeus/transport/middlewares.rb
+++ isomorfeus-transport/lib/isomorfeus/transport/middlewares.rb
@@ -2,9 +2,9 @@ module Isomorfeus
   module Transport
     module Middlewares
       def use_isomorfeus_middlewares
-        puts "Isomorfeus is using the following middlewares:"
+        puts "Isomorfeus is using the following middlewares:" if Isomorfeus.production?
         Isomorfeus.middlewares.each do |isomorfeus_middleware|
-          puts "#{isomorfeus_middleware}"
+          puts "#{isomorfeus_middleware}" if Isomorfeus.production?
           use isomorfeus_middleware
         end
       end
