8323a85937c51402ee0547f13c6a3d7225707af5
Jan Biedermann <jan@kursator.de>
2022-02-22 02:43:48 +0100

    2.2.3

--- isomorfeus-i18n/lib/isomorfeus/i18n/version.rb
+++ isomorfeus-i18n/lib/isomorfeus/i18n/version.rb
@@ -1,5 +1,5 @@
 module Isomorfeus
   module I18n
-    VERSION = '2.2.2'
+    VERSION = '2.2.3'
   end
 end
--- isomorfeus-operation/lib/isomorfeus/operation/version.rb
+++ isomorfeus-operation/lib/isomorfeus/operation/version.rb
@@ -1,5 +1,5 @@
 module Isomorfeus
   module Operation
-    VERSION = '2.2.2'
+    VERSION = '2.2.3'
   end
 end
