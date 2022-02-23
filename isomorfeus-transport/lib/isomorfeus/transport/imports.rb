903333d89aef20f38c8a95869be3eee99c36f920
Jan Biedermann <jan@kursator.de>
2022-02-23 11:29:41 +0100

    add SSR ruby import

--- isomorfeus-transport/lib/isomorfeus/transport/imports.rb
+++ isomorfeus-transport/lib/isomorfeus/transport/imports.rb
@@ -3,6 +3,7 @@ module Isomorfeus
     module Imports
       def self.add
         Isomorfeus.add_ssr_js_import('ws', 'WebSocket')
+        Isomorfeus.add_ssr_ruby_import('isomorfeus/transport/ssr_login')
       end
     end
   end
